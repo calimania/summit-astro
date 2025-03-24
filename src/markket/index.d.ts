b75c87d60bec1cf17b628160ee0b4858fc831d11
Daveed <daveed@santocabron.com>
2025-03-24 16:41:11 -0400

    Added individual event pages, better css for content blocks

--- src/markket/index.d.ts
+++ src/markket/index.d.ts
@@ -240,6 +240,37 @@ export interface Page {
   albums?: Album[];
 }
 
+export interface Event {
+  id: number;
+  documentId: string;
+  Name: string;
+  usd_price: number;
+  startDate: string;
+  Slides: Slide[];
+  endDate: string;
+  Description: string;
+  maxCapacity: number | null;
+  amountSold: number | null;
+  active: boolean;
+  STRIPE_PRODUCT_ID: string | null;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string;
+  SEO?: SEO;
+  Tag?: Tag[];
+  stores?: Store[];
+  Thumbnail?: {
+    url: string;
+    formats?: {
+      thumbnail?: { url: string };
+      small?: { url: string };
+      medium?: { url: string };
+      large?: { url: string };
+    };
+  };
+}
 
 export interface PageResponse {
   data: Page[];
